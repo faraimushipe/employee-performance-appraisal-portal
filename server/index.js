81cc4156f3036db58373d9427821dd2a316148e9
Mushipe Farai <mushipefarai01@gmail.com>
2025-11-24 14:44:56 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,6 +41,20 @@ app.use('/api/', limiter);
 // CORS disabled for school project
 // No CORS restrictions - allow all origins
 
+// Handle preflight requests
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 // Body parsing middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
