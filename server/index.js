84db3d27fd3d9d88586f0b8f95a03605e184e05f
Mushipe Farai <mushipefarai01@gmail.com>
2025-11-24 12:56:48 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ const { initializeDatabase } = require('./database/postgres');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Force deploy - updated timestamp v2
+console.log('Starting server with PostgreSQL database...');
+
 // Security middleware
 app.use(helmet());
 app.use(compression());
